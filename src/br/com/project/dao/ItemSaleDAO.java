d6f29d4ef313674f543102c9dac42490c60d9bdf
JoseFernandes975 <zehenrique752005@gmail.com>
2022-10-09 03:05:22 -0300

    Criado o método listar item de uma venda no ItemSalesDAO

--- src/br/com/project/dao/ItemSaleDAO.java
+++ src/br/com/project/dao/ItemSaleDAO.java
@@ -2,11 +2,15 @@
 package br.com.project.dao;
 import br.com.project.JDBC.ConnectionFactory;
 import br.com.project.model.Item_sales;
+import br.com.project.model.Product;
 import java.awt.event.FocusEvent;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import javax.swing.JOptionPane;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.table.DefaultTableModel;
 
 public class ItemSaleDAO {
@@ -51,6 +55,38 @@ public class ItemSaleDAO {
         }catch(SQLException e){
             JOptionPane.showMessageDialog(null, "Error delete item sale: "+e.getMessage());
         }
+    }
+    
+    public List<Item_sales> findItemBySale(int venda_id){
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        List<Item_sales> list = new ArrayList();
+        try{
+            st = conn.prepareStatement("SELECT i.Id, p.Descricao, i.Quantidade, p.Preco, i.Subtotal FROM tb_itensvendas AS i INNER "
+                    + "JOIN tb_produtos AS p ON (i.Produto_id = p.Id) WHERE i.Venda_id = ?");
+            
+            st.setInt(1, venda_id);
+            
+            rs = st.executeQuery();
+            
+            while(rs.next()){
+               Item_sales item = new Item_sales();
+               Product prod = new Product();
+               item.setId(rs.getInt("i.Id"));
+               prod.setDescription(rs.getString("p.Descricao"));
+               item.setQuantity(rs.getInt("i.Quantidade"));
+               prod.setPrice(rs.getDouble("p.Preco"));
+               item.setSubtotal(rs.getDouble("i.Subtotal"));
+               item.setProducts(prod);
+               
+               
+               list.add(item);
+            }
+            return list;
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Error: "+e.getMessage());
+        }
+        return list;
     }
 
     
